fb7ae3cb433e8be1577bca76f637f340924892e5
celloguy <will_wright24@yahoo.com>
2019-12-13 15:38:15 -0600

    Beginnings of functionality

---
+++ src/main/java/com/cs4743/Controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.cs4743.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    public LoginController(){
+
+    }
+
+    @FXML
+    TextField UsernameField;
+    @FXML
+    TextField passwordField;
+    @FXML
+    Button cancelButton;
+    @FXML
+    TextField loginButton;
+}
